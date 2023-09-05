417af97f12ff7b2e25a92a0f9da59cffe456f4e2
jcmunav63 <jcmunav63@gmail.com>
2023-09-05 16:57:49 -0600

    Edit cartSlice to correct error in remove item.

--- src/features/cart/cartSlice.js
+++ src/features/cart/cartSlice.js
@@ -17,7 +17,7 @@ const cartSlice = createSlice({
     },
     removeItem: (state, action) => {
       const itemId = action.payload;
-      state.cartItems = state-cartItems.filter((item) => item.id !== itemId);
+      state.cartItems = state.cartItems.filter((item) => item.id !== itemId);
     },
     increase: (state, {payload}) => {
       const cartItem = state.cartItems.find((item) => item.id === payload.id)
