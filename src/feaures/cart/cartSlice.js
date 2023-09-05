44359df85a80dc6925fc070c944322b75f1f547f
jcmunav63 <jcmunav63@gmail.com>
2023-09-04 19:59:51 -0600

    Setup React-Redux project, setup store & create cartSlice.

---
+++ src/feaures/cart/cartSlice.js
@@ -0,0 +1,18 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import cartItems from '../../cartItems';
+
+const initalState = {
+  cartItems: cartItems,
+  amount: 0,
+  total: 0,
+  isLoading: true,
+};
+
+const cartSlice = createSlice({
+  name: 'cart',
+  initialState,
+});
+
+// console.log(cartSlice);
+
+export default cartSlice.reducer;
