23f19260ab95baf70c3c1abba9362ad9d2617205
jcmunav63 <jcmunav63@gmail.com>
2023-09-05 09:42:23 -0600

    Add reducers and actions for remove all items & remove each item.

--- src/components/CartContainer.js
+++ src/components/CartContainer.js
@@ -1,7 +1,9 @@
 import CartItem from './CartItem';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
+import { clearCart } from '../features/cart/cartSlice';
 
 const CartContainer = () => {
+  const dispatch = useDispatch();
   const { cartItems, total, amount } = useSelector((store) => store.cart);
 
   if (amount < 1) {
@@ -29,7 +31,7 @@ const CartContainer = () => {
           Total <span>${total}</span>
         </h4>
       </div>
-      <button className='btn clear-btn'>
+      <button className='btn clear-btn' onClick={()=>dispatch(clearCart())}>
         Clear cart
       </button>
     </footer>
