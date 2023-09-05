e2819d15c4da7d9399ea8da7744df8e28b4fbcd2
jcmunav63 <jcmunav63@gmail.com>
2023-09-05 13:17:20 -0600

    Add modal slice and reducers for closing modal or cancelling.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import Navbar from './components/Navbar';
 import CartContainer from './components/CartContainer';
+import { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { calculateTotals } from './features/cart/cartSlice';
+import Modal from './components/Modal';
 
 function App() {
+  const { cartItems } = useSelector((store) => store.cart);
+  const { isOpen } = useSelector((store) => store.modal);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(calculateTotals());
+  }, [dispatch, cartItems]);
+
   return (
     <main>
+      {isOpen && <Modal />}
       <Navbar />
       <CartContainer />
     </main>
--- src/components/CartItem.js
+++ src/components/CartItem.js
@@ -1,10 +1,11 @@
 import { ChevronDown, ChevronUp} from '../icons';
-import { removeItem } from '../features/cart/cartSlice';
+import { removeItem, increase, decrease } from '../features/cart/cartSlice';
 import { useDispatch } from 'react-redux';
 
 const CartItem = ({id, img, title, price, amount}) => {
   const dispatch = useDispatch();
-  return <article className='cart-item'>
+  return (
+  <article className='cart-item'>
     <img src={img} alt={title} />
     <div>
       <h4>{title}</h4>
@@ -14,15 +15,24 @@ const CartItem = ({id, img, title, price, amount}) => {
       }}>Remove</button>
     </div>
     <div>
-      <button className='amount-btn'>
+      <button className='amount-btn' onClick={()=>{
+        dispatch(increase({id}));
+      }}>
         <ChevronUp />
       </button>
       <p className='amount'>{amount}</p>
-      <button className='amount-btn'>
+      <button className='amount-btn' onClick={()=>{
+        if(amount === 1) {
+          dispatch(removeItem(id))
+          return;
+        }
+        dispatch(decrease({id}))
+      }}>
         <ChevronDown />
       </button>
     </div>
   </article>
-}
+  );
+};
 
 export default CartItem;
